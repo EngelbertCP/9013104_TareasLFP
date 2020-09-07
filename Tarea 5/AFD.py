8c03e10d05f36aca8c13f5c0f1e566daf9b0b829
EngelberCP <9013104@ingenieria.usac.edu.gt>
2020-09-06 23:52:18 -0600

    Tarea 5

---
+++ Tarea 5/AFD.py
@@ -0,0 +1,19 @@
+cadena = 'aab1' #correcta
+cadena2 = 'aabb11b' #incorrecta
+
+def AFP(entrada):
+    estado=0
+
+    for i in range(len(entrada)):
+        if estado == 0:
+            if str(entrada[i]):
+                estado = 2
+            elif:
+                print("Error: cadena incorrecta")
+        elif estado == 1:
+        elif estado == 2:
+        elif estado == 3:
+        elif estado == 4:
+
+AFP(cadena)
+AFP(cadena2)
