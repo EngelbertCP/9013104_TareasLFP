715a02271d26248e6ac5017b8aa4c1dad4bfbc81
EngelberCP <9013104@ingenieria.usac.edu.gt>
2020-09-08 23:29:59 -0600

    Practica Unica

--- Practica Unica/MySimpleQL.py
+++ Practica Unica/MySimpleQL.py
@@ -190,11 +190,12 @@ while True:
 
                 TABLE = '''
                 <!DOCTYPE html>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
                 <html>
                     <head>
                     </head>
                     <body>
-                        <table>
+                        <table class="table table-bordered">
                             <tr>
                                 <th>Nombre</th>
                                 <th>Edad</th>
